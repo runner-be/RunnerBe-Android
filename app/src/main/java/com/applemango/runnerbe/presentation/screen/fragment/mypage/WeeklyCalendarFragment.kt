2900c48d1558a1a537466528006be3415e88a253
Minkyu Kim <lion9638@naver.com>
2024-10-21 18:22:58 +0900

    MyPage | 주간 러닝 로그 뷰페이저 추가 및 개선

--- app/src/main/java/com/applemango/runnerbe/presentation/screen/fragment/mypage/WeeklyCalendarFragment.kt
+++ app/src/main/java/com/applemango/runnerbe/presentation/screen/fragment/mypage/WeeklyCalendarFragment.kt
@@ -1,59 +1,108 @@
 package com.applemango.runnerbe.presentation.screen.fragment.mypage
 
-import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.applemango.runnerbe.R
+import com.applemango.runnerbe.RunnerBeApplication
+import com.applemango.runnerbe.data.network.response.RunningLog
 import com.applemango.runnerbe.databinding.FragmentWeeklyCalendarBinding
-import com.applemango.runnerbe.presentation.screen.dialog.stamp.StampItem
 import com.applemango.runnerbe.presentation.screen.fragment.base.BaseFragment
+import com.applemango.runnerbe.presentation.screen.fragment.main.MainFragmentDirections
 import com.applemango.runnerbe.presentation.screen.fragment.mypage.calendar.WeeklyCalendarAdapter
 import com.applemango.runnerbe.presentation.screen.fragment.mypage.calendar.initWeekDays
+import com.applemango.runnerbe.util.parseLocalDateToKorean
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class WeeklyCalendarFragment : BaseFragment<FragmentWeeklyCalendarBinding>(R.layout.fragment_weekly_calendar) {
+class WeeklyCalendarFragment(
+    private val position: Int
+) : BaseFragment<FragmentWeeklyCalendarBinding>(R.layout.fragment_weekly_calendar) {
 
     @Inject
     lateinit var weeklyCalendarAdapter: WeeklyCalendarAdapter
 
+    private val viewModel: MyPageViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initWeeklyCalendarAdapter()
+        setupWeeklyRunningLogs()
+    }
+
+    private fun setupWeeklyRunningLogs() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.thisWeekRunningLogFlow.collectLatest {
+                    val thisWeekStartDate = LocalDate.now().minusWeeks(POSITION_DEFAULT - position.toLong())
+                    val thisWeekLogs = parseRunningLogs(thisWeekStartDate, it.runningLog)
+                    weeklyCalendarAdapter.submitList(initWeekDays(thisWeekStartDate, thisWeekLogs))
+                }
+            }
+        }
+    }
+
+    private fun parseRunningLogs(
+        startDate: LocalDate,
+        runningLogs: List<RunningLog>
+    ): List<RunningLog> {
+        val thisWeekEndDate = startDate.plusDays(6)
+        return runningLogs
+            .filter {
+                it.runnedDate.toLocalDate() in startDate..thisWeekEndDate
+            }
     }
 
     private fun initWeeklyCalendarAdapter() {
         binding.rcvCalendarWeekly.apply {
             adapter = weeklyCalendarAdapter
-            val (startDate, weeklyStampList) = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                Pair(
-                    arguments?.getSerializable(ARG_DATE, LocalDate::class.java),
-                    arguments?.getParcelableArrayList(ARG_STAMP_LIST, StampItem::class.java)
-                )
-            } else {
-                Pair(
-                    arguments?.getSerializable(ARG_DATE) as LocalDate,
-                    arguments?.getParcelableArrayList(ARG_STAMP_LIST)
-                )
+            weeklyCalendarAdapter.setOnDateClickListener { item ->
+                viewModel.updateWeeklyViewPagerPosition(position)
+                val runningLog = item.runningLog
+
+                if (runningLog != null) {
+                    val userId = RunnerBeApplication.mTokenPreference.getUserId()
+                    navigate(
+                        MainFragmentDirections.actionMainFragmentToRunningLogDetailFragment(
+                            userId,
+                            runningLog.logId
+                        )
+                    )
+                } else {
+                    item.date?.let { itemDate ->
+                        navigate(
+                            MainFragmentDirections.actionMainFragmentToRunningLogFragment(
+                                parseLocalDateToKorean(itemDate),
+                                null,
+                                null
+                            )
+                        )
+                    }
+                }
             }
-//            weeklyCalendarAdapter.submitList(initWeekDays(startDate, weeklyStampList))
+
+            layoutManager = GridLayoutManager(context, 7)
         }
     }
 
     companion object {
         private const val ARG_DATE = "arg_date"
-        private const val ARG_STAMP_LIST = "arg_stamp_list"
+        private const val ARG_POSITION = "arg_position"
+        private const val POSITION_DEFAULT = 2
 
-        fun newInstance(date: LocalDate, stampList: List<StampItem>): WeeklyCalendarFragment {
-            val fragment = WeeklyCalendarFragment()
+        fun newInstance(date: LocalDate, position: Int): WeeklyCalendarFragment {
+            val fragment = WeeklyCalendarFragment(position)
             val args = Bundle()
             args.putSerializable(ARG_DATE, date)
-            args.putParcelableArrayList(ARG_STAMP_LIST, ArrayList(stampList))
+            args.putSerializable(ARG_POSITION, position)
             fragment.arguments = args
             return fragment
         }
