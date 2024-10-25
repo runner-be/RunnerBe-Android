66f27d59c3cf1b2c86be91a1ffafa6f83236dbd9
Minkyu Kim <lion9638@naver.com>
2024-10-25 19:00:23 +0900

    Util | 로그 유틸 개선

--- app/src/main/java/com/applemango/runnerbe/util/LogUtil.kt
+++ app/src/main/java/com/applemango/runnerbe/util/LogUtil.kt
@@ -4,7 +4,7 @@ import android.util.Log
 
 object LogUtil {
     private fun getTag(): String {
-        return Thread.currentThread().stackTrace
+        return Exception().stackTrace
             .firstOrNull {
                 it.className != LogUtil::class.java.name
                         && !it.className.contains("java.lang.Thread")
