0c15f0aaf526ce9cff0acd2fe481aa53ff02b1f1
Minkyu Kim <lion9638@naver.com>
2024-10-31 22:32:34 +0900

    Util | 코드 정리

--- app/src/main/java/com/applemango/runnerbe/presentation/screen/fragment/chat/detail/image/detail/ImageDetailUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.applemango.runnerbe.presentation.screen.fragment.chat.detail.image.detail
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class ImageDetailUiState(
-    val imageUrl :String?
-): Parcelable
