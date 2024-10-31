9e2b61a93663f8bbb19c768c7ba5dc6c4b44de26
Minkyu Kim <lion9638@naver.com>
2024-10-31 22:34:13 +0900

    RunningLog | 사진 촬영/앨범 선택 후 해당 이미지 크롭 기능 추가

---
+++ app/src/main/java/com/applemango/runnerbe/presentation/screen/fragment/image/CropRectRatio.kt
@@ -0,0 +1,9 @@
+package com.applemango.runnerbe.presentation.screen.fragment.image
+
+import com.applemango.runnerbe.R
+
+enum class CropRectRatio(val viewId: Int) {
+    RATIO_1_1(R.id.tv_ratio_1_1),
+    RATIO_4_3(R.id.tv_ratio_4_3),
+    RATIO_FILL(R.id.tv_ratio_fill)
+}
