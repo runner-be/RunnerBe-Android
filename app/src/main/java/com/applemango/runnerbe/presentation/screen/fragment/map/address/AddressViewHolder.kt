395a2e6adacce8df26a4aa3e8f9b0238511e42cb
Minkyu Kim <lion9638@naver.com>
2024-11-04 14:38:00 +0900

    PostingWrite | 주소 검색 결과 아이템 뷰홀더 데이터바인딩 적용

--- app/src/main/java/com/applemango/runnerbe/presentation/screen/fragment/map/address/AddressViewHolder.kt
+++ app/src/main/java/com/applemango/runnerbe/presentation/screen/fragment/map/address/AddressViewHolder.kt
@@ -1,6 +1,5 @@
 package com.applemango.runnerbe.presentation.screen.fragment.map.address
 
-import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import com.applemango.runnerbe.databinding.ItemAddressBinding
 
@@ -8,13 +7,9 @@ class AddressViewHolder (
     private val binding: ItemAddressBinding
 ): RecyclerView.ViewHolder(binding.root) {
     fun bind(item: AddressResult, onAddressClickListener: OnAddressClickListener) {
-        Log.e("Address search", " : $item")
-        with(binding) {
-            tvAddressMain.text = item.placeName
-            tvAddressSub.text = item.roadAddress
-            llAddress.setOnClickListener {
-                onAddressClickListener.onAddressClicked(item)
-            }
+        binding.item = item
+        binding.llAddress.setOnClickListener {
+            onAddressClickListener.onAddressClicked(item)
         }
     }
 }
