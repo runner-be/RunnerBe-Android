869c0cb060e81980d7def6f938147d77e4d85840
Minkyu Kim <lion9638@naver.com>
2024-11-12 22:24:44 +0900

    Mypage | 프로필 수정하기 클릭 시 화면 이동되지 않는 오류 수정

--- app/src/main/java/com/applemango/runnerbe/presentation/screen/fragment/mypage/paceinfo/PaceInfoViewModel.kt
+++ app/src/main/java/com/applemango/runnerbe/presentation/screen/fragment/mypage/paceinfo/PaceInfoViewModel.kt
@@ -12,24 +12,36 @@ import com.applemango.runnerbe.presentation.model.listener.PaceSelectListener
 import com.applemango.runnerbe.presentation.state.CommonResponse
 import com.applemango.runnerbe.presentation.state.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class PaceInfoViewModel @Inject constructor(val patchUserPaceUseCase: PatchUserPaceUseCase, val savedStateHandle: SavedStateHandle): ViewModel() {
+class PaceInfoViewModel @Inject constructor(
+    val patchUserPaceUseCase: PatchUserPaceUseCase,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
     val paceInfoList: MutableStateFlow<List<PaceSelectItem>> = MutableStateFlow(initPaceInfoList())
     val isConfirmButtonEnabled = combine(paceInfoList) { data ->
         data[0].any { it.isSelected }
-    }.stateIn(scope = viewModelScope, started = SharingStarted.WhileSubscribed(1000L),initialValue = false)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(1000L),
+        initialValue = false
+    )
     private val _paceInfoUiState: MutableLiveData<UiState> = MutableLiveData(UiState.Empty)
     val paceInfoUiState get() = _paceInfoUiState
 
-    val inputPage : String? = savedStateHandle["mode"]
+    val inputPage: String? = savedStateHandle["mode"]
 
     private val _action: MutableSharedFlow<PaceInfoRegistAction> = MutableSharedFlow()
-    val action :SharedFlow<PaceInfoRegistAction> get() = _action
-    fun getPaceInfoSelectListener() : PaceSelectListener = object : PaceSelectListener {
+    val action: SharedFlow<PaceInfoRegistAction> get() = _action
+    fun getPaceInfoSelectListener(): PaceSelectListener = object : PaceSelectListener {
         override fun itemClick(paceSelectItem: PaceSelectItem) {
             val list = ArrayList<PaceSelectItem>().apply {
                 addAll(paceInfoList.value.map { item ->
@@ -44,23 +56,31 @@ class PaceInfoViewModel @Inject constructor(val patchUserPaceUseCase: PatchUserP
     }
 
     fun backClicked() = viewModelScope.launch {
-       _action.emit(PaceInfoRegistAction.MoveToBack)
+        _action.emit(PaceInfoRegistAction.MoveToBack)
     }
+
     fun patchUserPace() {
         viewModelScope.launch {
             val userId = RunnerBeApplication.mTokenPreference.getUserId()
             val selectedPace = paceInfoList.value.firstOrNull { it.isSelected } ?: return@launch
             patchUserPaceUseCase(userId, selectedPace.pace).collect {
-                if(it is CommonResponse.Success<*>) {
+                if (it is CommonResponse.Success<*>) {
                     RunnerBeApplication.mTokenPreference.setMyRunningPace(selectedPace.pace.key)
-                    _action.emit(PaceInfoRegistAction.ShowCompleteDialog(selectedPace.pace, R.string.pace_complete_title, R.string.pace_complete_description))
+                    _action.emit(
+                        PaceInfoRegistAction.ShowCompleteDialog(
+                            selectedPace.pace,
+                            R.string.pace_complete_title,
+                            R.string.pace_complete_description
+                        )
+                    )
                 }
                 _paceInfoUiState.postValue(
-                    when(it) {
+                    when (it) {
                         is CommonResponse.Failed -> {
                             if (it.code <= 999) UiState.NetworkError
                             else UiState.Failed(it.message)
                         }
+
                         is CommonResponse.Loading -> UiState.Loading
                         else -> UiState.Empty
                     }
@@ -69,11 +89,15 @@ class PaceInfoViewModel @Inject constructor(val patchUserPaceUseCase: PatchUserP
         }
     }
 
-    }
+}
 
 sealed class PaceInfoRegistAction {
     object MoveToBack : PaceInfoRegistAction()
-    data class ShowCompleteDialog(val pace: Pace, val titleResource: Int, val descriptionResource: Int): PaceInfoRegistAction()
+    data class ShowCompleteDialog(
+        val pace: Pace,
+        val titleResource: Int,
+        val descriptionResource: Int
+    ) : PaceInfoRegistAction()
 }
 
 enum class PaceRegistrationInputPage(val mode: String) {
