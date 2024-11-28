1b7320825b4be0cb5ae78bb3291bfb0a5056c7b8
Minkyu Kim <lion9638@naver.com>
2024-11-28 23:43:15 +0900

    JoinedRunning | 버튼 3종류 활성화 로직 수정

--- app/src/main/java/com/applemango/runnerbe/presentation/screen/fragment/mypage/joinedrunning/JoinedRunningPostViewHolder.kt
+++ app/src/main/java/com/applemango/runnerbe/presentation/screen/fragment/mypage/joinedrunning/JoinedRunningPostViewHolder.kt
@@ -3,6 +3,8 @@ package com.applemango.runnerbe.presentation.screen.fragment.mypage.joinedrunnin
 import androidx.recyclerview.widget.RecyclerView
 import com.applemango.runnerbe.data.dto.Posting
 import com.applemango.runnerbe.databinding.ItemJoinPostWithBookmarkBinding
+import java.lang.IllegalArgumentException
+import java.time.ZonedDateTime
 
 class JoinedRunningPostViewHolder(
     private val binding: ItemJoinPostWithBookmarkBinding
@@ -13,13 +15,53 @@ class JoinedRunningPostViewHolder(
             clickListener = listener
             postFrom = from
 
-            val firstButtonVisibility = (item.whetherEnd == "N") || (item.whetherEnd == "Y" && !item.isRunningCaptain())
-            val secondButtonVisibility = item.whetherEnd == "Y" && item.isRunningCaptain()
-            val thirdButtonVisibility = item.whetherEnd == "D"
+            val isRunningEnd = try {
+                val gatheringTime = requireNotNull(item.gatheringTime)
+                val runningTime = requireNotNull(item.runningTime)
+                isRunningEnd(gatheringTime, runningTime)
+            } catch (e: IllegalArgumentException) {
+                e.printStackTrace()
+                false
+            }
+
+            val isThreeHourAfterRunningEnd = try {
+                val gatheringTime = requireNotNull(item.gatheringTime)
+                val runningTime = requireNotNull(item.runningTime)
+                isThreeHourAfterRunningEnd(gatheringTime, runningTime)
+            } catch (e: IllegalArgumentException) {
+                e.printStackTrace()
+                false
+            }
+
+            val firstButtonVisibility = !isRunningEnd
+            val secondButtonVisibility = isRunningEnd && item.isRunningCaptain() && !isThreeHourAfterRunningEnd
+            val thirdButtonVisibility = isRunningEnd && isThreeHourAfterRunningEnd
 
             runningEndTextViewVisibility = firstButtonVisibility
             attendanceCheckButtonVisibility = secondButtonVisibility
             attendanceSeeButtonVisibility = thirdButtonVisibility
         }
+    }
+
+    private fun isThreeHourAfterRunningEnd(
+        gatheringTime: ZonedDateTime,
+        runningTime: String
+    ): Boolean {
+        val runningTimeParts = runningTime.split(":").map { it.toInt() }
+        val runningDurationMinutes = runningTimeParts[0] * 60 + runningTimeParts[1]
+        val runningEndTime = gatheringTime.plusMinutes(runningDurationMinutes.toLong())
+        val currentTime = ZonedDateTime.now()
+         return currentTime.isAfter(runningEndTime.plusMinutes(180))
+    }
+
+    private fun isRunningEnd(
+        gatheringTime: ZonedDateTime,
+        runningTime: String
+    ): Boolean {
+        val runningTimeParts = runningTime.split(":").map { it.toInt() }
+        val runningDurationMinutes = runningTimeParts[0] * 60 + runningTimeParts[1]
+        val runningEndTime = gatheringTime.plusMinutes(runningDurationMinutes.toLong())
+        val currentTime = ZonedDateTime.now()
+        return currentTime.isAfter(runningEndTime)
     }
 }
