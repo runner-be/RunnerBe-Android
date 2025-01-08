448129c8d0ff72d95606b8ab86410a16d7463520
Minkyu Kim <lion9638@naver.com>
2025-01-08 16:49:40 +0900

    Common | Domain 모듈 분리

---
+++ domain/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java-library")
+    id("org.jetbrains.kotlin.jvm")
+    kotlin("kapt")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+dependencies {
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.46")
+    kapt("com.google.dagger:hilt-android-compiler:2.46")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+}
